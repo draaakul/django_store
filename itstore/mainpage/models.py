90236e20ad457cb526f3397aa1326c3aa5c3f394
Ivan Berestianyi <berestvania@gmail.com>
2023-12-21 02:07:15 +0200

    added new code in index.html, deleted old model (Products) > added new one (Product), added 8 items in database, create new html files (product, stars)

--- itstore/mainpage/models.py
+++ itstore/mainpage/models.py
@@ -1,12 +1,12 @@
-from colorfield.fields import ColorField
 from django.db import models
 
 
-class Products(models.Model):
+class Product(models.Model):
 
     name = models.CharField(max_length=100)
-    price = models.IntegerField()
+    price = models.DecimalField(max_digits=19, decimal_places=2)
     description = models.CharField(max_length=500)
+    image = models.ImageField(blank=True, upload_to='images')
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
 
--- itstore/mainpage/views.py
+++ itstore/mainpage/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404
-from .models import Products
+from .models import Product
 
 
 def index(request):
-    data = Products.objects.all()
+    products_data = Product.objects.all()
     context = {
-        'products': data,
+        'mainpage_products': products_data[:8],
+        'all_products': products_data,
+        'popular_products': products_data[:4],
+        'my_range': range(5),
+
     }
     return render(request, 'mainpage/index.html', context=context)
 
